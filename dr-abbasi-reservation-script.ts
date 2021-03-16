72b48dcaf05ad4d956919dfa4bfeefc0a9ced5c0
Ali Rostami <ali.rostami@live.com>
2021-03-16 21:36:36 +0330

    working script that can fetch captcha and ask you to enter it, it will take care of the rest

--- dr-abbasi-reservation-script.ts
+++ dr-abbasi-reservation-script.ts
@@ -1,9 +1,22 @@
 import axios from 'axios'
 import * as fs from 'fs';
+import readline from 'readline'
 
-async function submit() {
+async function askQuestion(query: string): Promise<string> {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
 
-	axios({
+	return new Promise(resolve => rl.question(query, ans => {
+		rl.close();
+		resolve(ans);
+	}))
+}
+
+async function submit(captcha: string) {
+
+	return axios({
 		method: 'post',
 		url: "http://doctorabbasi.ir/application/views/ajax/add.php",
 		headers: {
@@ -14,25 +27,12 @@ async function submit() {
 			"x-requested-with": "XMLHttpRequest",
 			"cookie": "PHPSESSID=fl9u7disbest4mediq29igvpd1",
 		},
-		data: "act=addReserve&firstname=فاطمه&family=شریفی&nationalId=0858679426&year=0&active=1&gender=female&mobile=09155571756&phone=0&capcha=86501&scheduleId=74933&serviceType=17&reserveGroupId=0&callingPage=patient&reserveType=1&come=1&age=0&fatherName=&address=&reserveTimeSpaceFlag=0&dossierId=",
+		data: `act=addReserve&firstname=فاطمه&family=شریفی&nationalId=0858679426&year=0&active=1&gender=female&mobile=09155571756&phone=0&capcha=${captcha}&scheduleId=74933&serviceType=17&reserveGroupId=0&callingPage=patient&reserveType=1&come=1&age=0&fatherName=&address=&reserveTimeSpaceFlag=0&dossierId=`,
 	}).then((response) => console.log(response))
 }
 
-// submit()
-
-
 async function getCaptcha() {
-	// const captchaResult = await fetch("http://doctorabbasi.ir/application/views/create_image.php?0.8053264878919875", {
-	// 	"headers": {
-	// 		"accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-	// 		"accept-language": "en-US,en;q=0.9,fa;q=0.8",
-	// 		"cookie": "PHPSESSID=fl9u7disbest4mediq29igvpd1",
-	// 	},
-	// 	"body": undefined,
-	// 	"method": "GET",
-	// });
-
-	axios({
+	return axios({
 		method: "get",
 		url: "http://doctorabbasi.ir/application/views/create_image.php?0.8053264878919875",
 		headers: {
@@ -45,7 +45,14 @@ async function getCaptcha() {
 		response.data.pipe(fs.createWriteStream('./dist/captcha.jpg'))
 		console.log("captcha saved!")
 	})
-
 }
 
-getCaptcha();
+
+async function main() {
+	await getCaptcha()
+	const captcha = await askQuestion('Enter the captcha: ')
+	await submit(captcha);
+}
+
+main()
+
