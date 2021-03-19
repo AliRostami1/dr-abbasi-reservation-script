a052b2d18aeac2c89a2b823b8084def49546e980
Ali Rostami <ali.rostami@live.com>
2021-03-19 22:20:08 +0330

    Added prettier and did some styling

--- src/dr-abbasi-reservation-script.ts
+++ src/dr-abbasi-reservation-script.ts
@@ -1,43 +1,41 @@
-import axios from 'axios';
-import * as fs from 'fs';
-import path from 'path';
+import axios from "axios";
+import * as fs from "fs";
+import path from "path";
 
 interface Data {
-	firstName: string,
-	lastName: string,
-	nationalId: string,
-	gender: string,
-	mobile: string,
-	cookie: string,
-	captcha: string
+  firstName: string;
+  lastName: string;
+  nationalId: string;
+  gender: string;
+  mobile: string;
+  cookie: string;
+  captcha: string;
 }
-const pathToJson = path.resolve(__dirname, '../config.json');
-const data: Data = JSON.parse(fs.readFileSync(pathToJson, 'utf8'));
+const pathToJson = path.resolve(__dirname, "../config.json");
+const data: Data = JSON.parse(fs.readFileSync(pathToJson, "utf8"));
 
 async function submit() {
-	return axios({
-		method: 'post',
-		url: "http://doctorabbasi.ir/application/views/ajax/add.php",
-		headers: {
-			"accept": "text/javascript, text/html, application/xml, text/xml, */*",
-			"accept-language": "en-US,en;q=0.9,fa;q=0.8",
-			"content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-			"x-prototype-version": "1.7",
-			"x-requested-with": "XMLHttpRequest",
-			"cookie": data.cookie,
-		},
-		data: `act=addReserve&firstname=${data.firstName}&family=${data.lastName}&nationalId=${data.nationalId}&year=0&active=1&gender=${data.gender}&mobile=${data.mobile}&phone=0&capcha=${data.captcha}&scheduleId=74933&serviceType=17&reserveGroupId=0&callingPage=patient&reserveType=1&come=1&age=0&fatherName=&address=&reserveTimeSpaceFlag=0&dossierId=`,
-	})
+  return axios({
+    method: "post",
+    url: "http://doctorabbasi.ir/application/views/ajax/add.php",
+    headers: {
+      accept: "text/javascript, text/html, application/xml, text/xml, */*",
+      "accept-language": "en-US,en;q=0.9,fa;q=0.8",
+      "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      "x-prototype-version": "1.7",
+      "x-requested-with": "XMLHttpRequest",
+      cookie: data.cookie,
+    },
+    data: `act=addReserve&firstname=${data.firstName}&family=${data.lastName}&nationalId=${data.nationalId}&year=0&active=1&gender=${data.gender}&mobile=${data.mobile}&phone=0&capcha=${data.captcha}&scheduleId=74933&serviceType=17&reserveGroupId=0&callingPage=patient&reserveType=1&come=1&age=0&fatherName=&address=&reserveTimeSpaceFlag=0&dossierId=`,
+  });
 }
 
 async function main() {
-	const cancelInterval = setInterval(async () => {
-		const res = await submit();
-		console.log(res.data)
-		if (res.data.returnResult !== 'fullList') clearInterval(cancelInterval);
-	}, 1000)
-
+  const cancelInterval = setInterval(async () => {
+    const res = await submit();
+    console.log(res.data);
+    if (res.data.returnResult !== "fullList") clearInterval(cancelInterval);
+  }, 1000);
 }
 
-main()
-
+main();
--- src/setup-script.ts
+++ src/setup-script.ts
@@ -1,82 +1,85 @@
-import axios from 'axios'
-import * as fs from 'fs';
-import readline from 'readline'
-import path from 'path';
+import axios from "axios";
+import * as fs from "fs";
+import readline from "readline";
+import path from "path";
 
 interface Data {
-    firstName: string,
-    lastName: string,
-    nationalId: string,
-    gender: string,
-    mobile: string,
-    cookie: string,
-    captcha: string
+  firstName: string;
+  lastName: string;
+  nationalId: string;
+  gender: string;
+  mobile: string;
+  cookie: string;
+  captcha: string;
 }
 
-
 async function askQuestion(query: string): Promise<string> {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-    return new Promise(resolve => rl.question(query, ans => {
-        rl.close();
-        resolve(ans);
-    }))
+  return new Promise((resolve) =>
+    rl.question(query, (ans) => {
+      rl.close();
+      resolve(ans);
+    })
+  );
 }
 
 async function getCaptcha() {
-    return axios({
-        method: "get",
-        url: "http://doctorabbasi.ir/application/views/create_image.php?0.8053264878919875",
-        headers: {
-            "accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-            "accept-language": "en-US,en;q=0.9,fa;q=0.8"
-        },
-        responseType: "stream"
-    }).then((response) => {
-        response.data.pipe(fs.createWriteStream('./dist/captcha.jpg'))
-        console.log("Captcha Saved!")
-        return response;
-    })
+  return axios({
+    method: "get",
+    url:
+      "http://doctorabbasi.ir/application/views/create_image.php?0.8053264878919875",
+    headers: {
+      accept:
+        "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+      "accept-language": "en-US,en;q=0.9,fa;q=0.8",
+    },
+    responseType: "stream",
+  }).then((response) => {
+    response.data.pipe(fs.createWriteStream("./dist/captcha.jpg"));
+    console.log("Captcha Saved!");
+    return response;
+  });
 }
 
 async function main() {
-    const pathToJson = path.resolve(__dirname, '../config.json');
+  const pathToJson = path.resolve(__dirname, "../config.json");
 
-    let data: Data;
+  let data: Data;
 
-    if (!fs.existsSync(pathToJson)) {
-        const firstName = await askQuestion("Enter your First Name: ");
-        const lastName = await askQuestion("Enter your Last Name: ");
-        const nationalId = await askQuestion("Enter your nationalId: ");
-        const gender = await askQuestion("Enter your gender(male/female): ");
-        const mobile = await askQuestion("Enter your mobile number: ");
+  if (!fs.existsSync(pathToJson)) {
+    const firstName = await askQuestion("Enter your First Name: ");
+    const lastName = await askQuestion("Enter your Last Name: ");
+    const nationalId = await askQuestion("Enter your nationalId: ");
+    const gender = await askQuestion("Enter your gender(male/female): ");
+    const mobile = await askQuestion("Enter your mobile number: ");
 
-        data = {
-            firstName,
-            lastName,
-            nationalId,
-            gender,
-            mobile,
-            cookie: "",
-            captcha: ""
-        };
-        fs.writeFileSync(pathToJson, JSON.stringify(data), { flag: "w" })
-    }
+    data = {
+      firstName,
+      lastName,
+      nationalId,
+      gender,
+      mobile,
+      cookie: "",
+      captcha: "",
+    };
+    fs.writeFileSync(pathToJson, JSON.stringify(data), { flag: "w" });
+  }
 
-    data = JSON.parse(fs.readFileSync(pathToJson, 'utf8'));
+  data = JSON.parse(fs.readFileSync(pathToJson, "utf8"));
 
-    const res = await getCaptcha();
+  const res = await getCaptcha();
 
-    data.cookie = res.headers["set-cookie"]
-    data.captcha = await askQuestion("A new captcha.jpg is saved at ./dist/captcha.jpg, Please enter it's number: ");
+  data.cookie = res.headers["set-cookie"];
+  data.captcha = await askQuestion(
+    "A new captcha.jpg is saved at ./dist/captcha.jpg, Please enter it's number: "
+  );
 
-    const jsonData = JSON.stringify(data);
-    fs.writeFileSync(pathToJson, jsonData);
+  const jsonData = JSON.stringify(data);
+  fs.writeFileSync(pathToJson, jsonData);
 }
 
 main();
-
-
